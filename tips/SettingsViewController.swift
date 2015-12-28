ae2051829a5010e0f7b4b21ae729950f530dcd4b
isisanchalee <isis.wenger@onelogin.com>
2015-12-27 17:35:20 -0800

    Add more styles

---
+++ tips/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Isis Anchalee on 12/27/15.
+//  Copyright © 2015 Isis Anchalee. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBAction func SettingsBack(sender: AnyObject) {
+      dismissViewControllerAnimated(true, completion: nil)
+    }
+}
